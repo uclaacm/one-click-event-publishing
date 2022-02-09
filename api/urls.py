8a16d14bb06940e9c267aa9ee240a27e3004321e
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-02-09 10:32:39 -0800

    renamed a function and endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import main
+from .views import create_event
 
 urlpatterns = [
-    path('',  main)
+    path('/create-event',  create_event)
 ]
--- api/views.py
+++ api/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("hello")
+def create_event(request):
+    return 0
