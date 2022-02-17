d2280fb45dc2cefe7b5c60c03e5581ee4244d084
Isha <isha@MacBook-Air.local>
2022-02-16 17:45:52 -0800

    added home

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import main
+
+from .views import main, signin,home
+
 
 urlpatterns = [
-    path('',  main)
+    path('',  main),
+    path('signin/',signin, name="signin"),
+    path('home/',home,name="home")
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+#from rest_framework.decorators import api_view
 # Create your views here.
 
+#@api_view(['GET','POST'])
 def main(request):
-    return HttpResponse("hello")
+    routes=[
+        {
+            'Endpoint':'/signin',
+            'method':'POST',
+            'body':{'password':""},
+            'description':'Sign in for website'
+        },
+        {
+            'Endpoint':'/home',
+            'method':'GET',
+            'body':None,
+            'description':'Sign in for website'
+        }
+    ]
+    return HttpResponse(routes)
+
+# @api_view(['POST'])
+def signin(request):
+    return HttpResponse('Signed in')
+
+def home(request):
+    return HttpResponse('Event home')
