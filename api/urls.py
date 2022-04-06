a329c6b1bd1bd09aec5d9f9e8ff6dbaa8d7296b7
Isha Rajput <33170219+irajput@users.noreply.github.com>
2022-04-05 21:18:25 -0700

    added facebook api (#13)
    Added function that creates posts on facebook. Doesn't support creating events yet, for now.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import create_event
+from .utilities import facebook_graph
 
 urlpatterns = [
-    path('create-event',  create_event)
+    path('create-event',  create_event),
+    path('post-facebook',facebook_graph)
 ]
