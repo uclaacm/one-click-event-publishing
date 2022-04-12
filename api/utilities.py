e4b1054ab4942fd9ba19db4693dd0f1534f1541a
KhanhZo Ball <77855101+khanhzoball@users.noreply.github.com>
2022-04-12 14:49:43 -0700

    created discord event function (#12)
    * created discord event function
    
    * changed secret token

--- api/utilities.py
+++ api/utilities.py
@@ -1,10 +1,34 @@
-
+from requests import get, post
 import facebook as fb
 from django.http import HttpResponse
-from requests import get, post
 import os
 from dotenv import load_dotenv
 
+def create_discord_event(name, description, start_time, end_time, location, image):
+    acm_guild_id = os.getenv('ACM_GUILD_ID')
+    acm_bot_token = os.getenv('ACM_BOT_TOKEN')
+    api_url = "https://discord.com/api/guilds/" + acm_guild_id + "/scheduled-events"
+
+    data = {
+        'name': name,
+        'description': description,
+        'privacy_level': '2',
+        'scheduled_start_time': start_time,
+        'scheduled_end_time': end_time,
+        'entity_type': '3',
+        'entity_metadata': {'location': location},
+        'image': image,
+    }
+
+    headers = {
+        'Authorization': 'Bot %s' % (acm_bot_token),
+        'Content-Type': 'application/json',
+    }
+
+    created_event = post(api_url, json = data, headers = headers).json()
+
+    return created_event
+  
 
 def facebook_graph(request):
     load_dotenv()
@@ -53,6 +77,3 @@ def instagram_graph(description, image_url):
     response = post(url, headers=headers, json=body).json()
 
     return HttpResponse("Successfully posted to facebook!")
-    
-
-
--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
+from tracemalloc import start
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utilities import create_discord_event
+import json
+
 
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
@@ -15,4 +19,25 @@ def testJwt(request):
     return HttpResponse("TEST")
 
 def create_event(request):
+    data = json.loads(request.body.decode('utf-8'))
+
+    name = data['name']
+    description = data['description']
+    start_time = data['start_time']
+    end_time = data['end_time']
+    location = data['location']
+    image = data['image']
+
+
+    create_discord_event(
+        name = name,
+        description = description,
+        start_time = start_time,
+        end_time = end_time,
+        location = location,
+        image = image,
+    )
+
     return HttpResponse("hello")
+
+
