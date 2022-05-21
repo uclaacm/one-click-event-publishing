62e30f7effafd5d279ea51a00f79658e81132141
Amy He <39035908+Amyh11325@users.noreply.github.com>
2022-05-21 11:12:36 -0700

    added authorize-token route (#30)
    * added authorize-token route, a little buggy on my end
    
    * added cors
    
    * changed http error code for missing env variables

--- backend/route_config.py
+++ backend/route_config.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/", methods=['GET'])
 def hello():
